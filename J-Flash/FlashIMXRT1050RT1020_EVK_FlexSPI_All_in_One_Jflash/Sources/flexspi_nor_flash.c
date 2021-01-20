be8993f65e4ac558a54aca47d3c267dd2d6a5ef2
jicheng0622 <jicheng0622@163.com>
2021-01-20 18:36:41 +0800

    Change Rx loopback mode from dqs pad to internally
    Keep LoopbackInternally mode to avoid the case dqs pad is not initliazed in SerialDownload boot mode or another case the pin is used on customer's board

--- J-Flash/FlashIMXRT1050RT1020_EVK_FlexSPI_All_in_One_Jflash/Sources/flexspi_nor_flash.c
+++ J-Flash/FlashIMXRT1050RT1020_EVK_FlexSPI_All_in_One_Jflash/Sources/flexspi_nor_flash.c
@@ -1609,7 +1609,11 @@ status_t parse_sfdp(uint32_t instance,
             break;
         }
 
-        config->memConfig.readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackFromDqsPad;
+        /* 
+           Keep LoopbackInternally mode to avoid the case dqs pad is not initliazed 
+           in SerialDownload bootmode or another case the pin is used on customer's board 
+        */
+        config->memConfig.readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackInternally;
 
         // Write Enable
         config->memConfig.lookupTable[4 * NOR_CMD_LUT_SEQ_IDX_WRITEENABLE] =
